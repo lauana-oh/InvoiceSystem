f72251a1cdfb39e77626bc7e133ccac80b96dcdd
lau-hubler <lauanahubler@gmail.com>
2019-11-14 02:39:08 -0500

    Created client model and migration

---
+++ database/migrations/2019_11_14_072655_create_clients_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->integer('nit')->unique();
+            $table->string('email');
+            $table->integer('phone');
+            $table->string('address');
+            $table->string('city');
+            $table->string('region');
+            $table->string('country');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
